de869a410f5e94093ac370a5eaead959427c24f8
Aurora <programming.aurora@gmail.com>
2022-10-11 18:05:15 +0100

    Added absolute imports.

--- entrega_2/app/src/main.tsx
+++ entrega_2/app/src/main.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "app/App";
 import {
 	createBrowserRouter,
 	RouterProvider,
 } from "react-router-dom";
 import { CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@mui/system";
-import { theme } from "./app/theme";
-import Rooms from "./features/rooms/Rooms";
-import Main from "./app/Main";
+import { theme } from "app/theme";
+import Rooms from "features/rooms/Rooms";
+import Main from "app/Main";
 
 const router = createBrowserRouter([
 	{
